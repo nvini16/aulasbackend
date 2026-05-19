8c84d1f38e29b9a69073b0ce26184a795adb0546
Nathanael Vinicius <nathanael.vinicius03@gmail.com>
2026-05-19 20:50:48 -0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,21 @@
+
+
+
+for (let i=0; i<=8; i++){
+    console.log(`Verificando e limpando: ${i}`);
+
+}
+
+
+// licao  dois 
+
+function statusCozinha(){
+    for (let nome=1; nome <20; nome++){
+        if(nome %2 == 0){
+            console.log("Pedido VIP saindo!")
+
+        }else {
+            console.log("Pedido comum pronto!")
+        }
+    }
+}
---
+++ rodar Node.bat
@@ -0,0 +1,4 @@
+@echo off
+set PATH=%PATH%;"C:\Users\NathanaelViniciusGom\Downloads\node-v24.14.1-win-x64\node-v24.14.1-win-x64"
+cd "C:\Users\NathanaelViniciusGom\Documents\aula_backend"
+cmd /k
